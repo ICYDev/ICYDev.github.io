6ea146fd22208cb4566b1ec6b0a1b14caef93c2f
ICYDev <51445618+ICYDev@users.noreply.github.com>
2023-03-15 21:31:40 +0800

    Chained effects together for preLoad and fade in content

--- main.js
+++ main.js
@@ -1,42 +1,48 @@
 //Text Glich Cycle Effect
-document.querySelectorAll("h1").forEach((element) => {
-    element.onmouseover = event => {
-        const iterationPerChar = 2;
-        let iteration = 0
-        const interval = setInterval(() => {
-            event.target.textContent = event.target.textContent.split("")
-                .map((letter, index) => {
-                    if(index < iteration) {
-                        return event.target.dataset.value[index]; 
-                    }
-                    return (Math.floor(Math.random() * 36)).toString(36).toUpperCase()
-                })
-                .join("");
+const textCycle = element => {
+    const iterationPerChar = 2;
+    let iteration = 0
+    const interval = setInterval(() => {
+        element.textContent = element.dataset.value.split("")
+            .map((letter, index) => {
+                if(index + 1 < iteration || element.dataset.value[index] == " ") {
+                    return element.dataset.value[index]; 
+                }
+                return (Math.floor(Math.random() * 36)).toString(36).toUpperCase()
+            })
+            .join("");
 
-            if(iteration >= event.target.dataset.value.length) clearInterval(interval);
-            iteration += 1 / iterationPerChar;
-        }, 40)
-    }
+        if(iteration > element.dataset.value.length) clearInterval(interval);
+        iteration += 1 / iterationPerChar;
+    }, 40)
+}
+
+document.querySelectorAll(".hoverGlichText").forEach((element) => {
+    element.onmouseover = event => textCycle(event.target);
 })
 
 //Grid Stagger Effect
 const wrapper = document.getElementById("tiles");
 
-
-
 let columns = Math.floor(document.body.clientWidth / 50),
     rows = Math.floor(document.body.clientHeight / 50),
     gridVisable = true;
 
-const handleOnClick = index => {
-    gridVisable = !gridVisable;
+const togglePreloadVisable = visable => {
+    gridVisable = visable === undefined ? !gridVisable : visable;
     anime({
         targets: ".tile",
         opacity: gridVisable ? 1 : 0,
-        delay: anime.stagger(50, {
-            grid: [columns, rows]
+        delay: anime.stagger(100, {
+            grid: [columns, rows],
+            from: Math.random() * document.querySelectorAll(".tile").length
         })
-    })
+    });
+    anime({
+        targets: ".loadElement",
+        opacity: gridVisable ? 1 : 0,
+        duration: document.querySelectorAll(".tile").length * 50
+    });
 }
 
 const createTile = index => {
@@ -44,7 +50,10 @@ const createTile = index => {
 
     tile.classList.add("tile");
 
-    tile.onclick = e => handleOnClick(index);
+    tile.style.opacity = gridVisable ? 1 : 0;
+    tile.style.backgroundColor = '#12' + (Math.floor(index%11)+16).toString(16) + '3B';
+
+    /*tile.onclick = e => togglePreloadVisable(index);*/
 
     return tile;
 }
@@ -70,3 +79,27 @@ const createGrid = () => {
 
 createGrid();
 window.onresize = () => createGrid();
+
+//PreLoad Animation
+const textList = [
+    ["Creating", "Designing", "Coding", "Animating", "Balancing"],
+    ["Player", "Enemy", "Weapon", "Terrain"],
+    ["Stats", "Behaviour", "Movement", "Lighting", "Generation"]
+];
+
+var loadTextElements = document.querySelectorAll(".loadScreenText");
+const cycleText = () => {
+    cycleTargetIndex = Math.floor(Math.random()*loadTextElements.length); 
+    target = loadTextElements[cycleTargetIndex];
+    while(target.dataset.value == target.textContent){
+        target.dataset.value = textList[cycleTargetIndex][Math.floor(Math.random() * textList[cycleTargetIndex].length)];
+    }
+    textCycle(target);
+};
+setInterval(cycleText, 1250);
+
+window.onload = () => {
+    togglePreloadVisable(false);
+}
+
+setInterval(togglePreloadVisable,5000)
